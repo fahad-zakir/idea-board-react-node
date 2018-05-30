14c6d1020fe007b82554d7165661f2186268ed2c
fahad-zakir <fahadzakir81@gmail.com>
2018-05-30 17:55:05 -0400

    created controllers folder

--- server.js
+++ server.js
@@ -31,3 +31,7 @@ app.use(express.static(__dirname + '/client/build/'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/build/index.html')
 })
+
+const user = require('./controllers/userController')
+app.use('/api/users', user)
+
