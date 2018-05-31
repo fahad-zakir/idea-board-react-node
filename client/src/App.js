dd3247278038138d8a2f36831d72e8e03fb6a39a
fahad-zakir <fahadzakir81@gmail.com>
2018-05-30 20:03:01 -0400

    added bootstrap and imported on index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import styled from 'styled-components'
+import "./App.css";
+
 import HomePage from './components/HomePage'
 import LogInPage from './components/LogInPage'
 import IdeaPage from './components/IdeaPage'
