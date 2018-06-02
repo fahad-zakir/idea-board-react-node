2a887dd21c6b839f8419f14992160b938330545d
fahad-zakir <fahadzakir81@gmail.com>
2018-06-01 23:48:19 -0400

    added home link to login page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+
 class HomePage extends Component {
     render() {
         return (
             <div>
                 <h1 className="title">React-Idea-Board</h1>
                 <h2 className="login"></h2>
+
             </div>
         );
     }
--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 class LogInPage extends Component {
     render() {
         return (
             <div>
+                <div className="styling">
+                    <Link to="/">Home</Link>
+                </div>
                <h1>Nicely Done!</h1> 
             </div>
         );
